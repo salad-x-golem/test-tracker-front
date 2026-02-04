7a1f64733236987716abab70a9f1d268124084d0
scx1332 <sieciech@gmail.com>
2026-02-04 21:02:48 +0100

    Working on test list

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { HashRouter, Route, Routes } from "react-router-dom";
 import { AppLayout } from "@/components/layout";
 import { ThemeProvider } from "@/components/theme-provider";
-import { MachineDetailPage, MachinesPage } from "@/features/machines";
 import { OverviewPage } from "@/features/overview";
 
 function App() {
@@ -11,11 +10,6 @@ function App() {
         <AppLayout>
           <Routes>
             <Route path="/" element={<OverviewPage />} />
-            <Route path="/machines" element={<MachinesPage />} />
-            <Route
-              path="/machines/:machineId"
-              element={<MachineDetailPage />}
-            />
           </Routes>
         </AppLayout>
       </HashRouter>
